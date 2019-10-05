176c4603722d2bd75bd6cc399d05f1e8b888c425
zalam003 <48445346+zalam003@users.noreply.github.com>
2019-10-05 16:28:23 -0400

    Update run_windows.bat

--- publictest/scripts/run_windows.bat
+++ publictest/scripts/run_windows.bat
@@ -1,16 +1,15 @@
 @echo OFF
 
 ::####################################################################
-:: Description: This script is to aide in setting up Energi 3.x aka
-::              Gen3 in a Unix environment
+:: Description: This script is to start Energi 3.x aka Gen3 in
+::              Windows environment
 ::
 :: Run this script
 :: run_windows.bat
 ::####################################################################
 
-
-
 set "VERSION=0.5.5"
+
 set "INSTALL_DIR=C:\Apps\EnergiCore3"
 set "DATA_DIR=EnergiCore3\testnet\energi3"
 set "BLOCKCHAIN_DIR=%userprofile%\AppData\Roaming\%DATA_DIR%"
@@ -20,3 +19,4 @@ set "DEFAULT_EXE_LOCATION=%INSTALL_DIR%\energi3-windows-4.0-amd64.exe"
 cd "%INSTALL_DIR%"
 
 @echo Starting Energi Core Node %VERSION%
+%windir%\system32\cmd.exe /c %DEFAULT_EXE_LOCATION% --testnet console 2> %DATA_DIR%\enegi3debug.log
