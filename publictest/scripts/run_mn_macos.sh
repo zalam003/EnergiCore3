bb27d687fce26f99e53c533aac87c3b96109d3f8
zalam003 <48445346+zalam003@users.noreply.github.com>
2019-10-06 21:13:34 -0400

    Create run_mn_macos.sh

---
+++ publictest/scripts/run_mn_macos.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+#####################################################################
+# Description: This script is to start energicore3 public test node
+#              masternode server
+#
+# Download this script
+# curl -sL https://raw.githubusercontent.com/zalam003/EnergiCore3/master/publictest/scripts/run_macos.sh > run_macos.sh
+#####################################################################
+#
+
+export PATH=$PATH:$HOME/Downloads
+
+# Create directory for logfile
+if [ ! -d "${HOME}/Library/Application Support/EnergiCore3/testnet/log" ]
+then
+        mkdir -p "${HOME}/Library/Application Support/EnergiCore3/testnet/log"
+fi
+
+# Install dig which is part of bind
+#brew install bind
+
+# Make executable
+if [ ! -x $HOME/Downloads/energi3-darwin-10.6-amd64 ]
+then
+    chmod +x $HOME/Downloads/energi3-darwin-10.6-amd64
+fi
+
+# Set variables
+LOGFILE="${HOME}/Library/Application Support/EnergiCore3/testnet/log/energicore3.log"
+IP=`dig +short myip.opendns.com @resolver1.opendns.com`
+
+if [ "x$IP" != "x" ]
+then
+   $HOME/Downloads/energi3-darwin-10.6-amd64 \
+        --masternode \
+        --nat extip:${IP} \
+        --testnet \
+        --mine \
+        --rpcapi admin,eth,web3,rpc,personal \
+        --rpc \
+        --rpcport 49796 \
+        --rpcaddr "127.0.0.1" \
+        --verbosity 3 \
+        console 2>> $LOGFILE
+else
+    echo "Cannot determine external IP address"
+    $HOME/Downloads/energi3-darwin-10.6-amd64 \
+        --masternode \
+        --testnet \
+        --mine \
+        --rpcapi admin,eth,web3,rpc,personal \
+        --rpc \
+        --rpcport 49796 \
+        --rpcaddr "127.0.0.1" \
+        --verbosity 3 \
+        console 2>> $LOGFILE
+fi
