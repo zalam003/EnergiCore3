13874dbdbaa2b43f74fd5f84c83a979564bc3261
zalam003 <48445346+zalam003@users.noreply.github.com>
2019-10-20 11:13:04 -0400

    Update utils.js

--- publictest/js/utils.js
+++ publictest/js/utils.js
@@ -8,3 +8,14 @@ function checkAllBalances() {
     }
     console.log("  Total balance: " + totalBal + " NRG");
 };
+
+function mnBalances() {
+    var totalBal = 0;
+    for (var acctNum in eth.accounts) {
+        var acct = eth.accounts[acctNum];
+        var acctBal = web3.fromWei(masternode.masternodeInfo(acct).collateral, "ether");
+        totalBal += parseFloat(acctBal);
+        console.log("  mn.accounts[" + acctNum + "]: \t" + acct + " \tbalance: " + acctBal + " NRG");
+    }
+    console.log("  Total balance: " + totalBal + " NRG");
+};
