a36b548281190316b14b92b1614e2dd1fd50ae74
zalam003 <48445346+zalam003@users.noreply.github.com>
2019-11-04 11:49:23 -0500

    Update run_mn_windows.bat

--- publictest/scripts/run_mn_windows.bat
+++ publictest/scripts/run_mn_windows.bat
@@ -8,19 +8,20 @@
 :: Run script:  run_mn_windows.bat
 ::####################################################################
 
-set "VERSION=0.5.5"
+set "VERSION=0.5.7"
 
-:: energi3-windows-4.0-amd64.exe version >version.tmp
+:: energi3.exe version >version.tmp
 :: findstr /B Version version.tmp
 :: del version.tmp
 
-set "INSTALL_DIR=C:\EnergiCore3"
+set "BIN_DIR=C:\energi3\bin"
 set "DATA_DIR=EnergiCore3\testnet\energi3"
-set "BLOCKCHAIN_DIR=%userprofile%\AppData\Roaming\%DATA_DIR%"
-set "DEFAULT_EXE_LOCATION=%INSTALL_DIR%\energi3-windows-4.0-amd64.exe"
+set "LOG_DIR=%APPDATA%\EnergiCore3\testnet\log"
+set "BLOCKCHAIN_DIR=%APPDIR%\%DATA_DIR%"
+set "DEFAULT_EXE_LOCATION=%BIN_DIR%\energi3.exe"
 
 @echo Changing to install directory
-cd "%INSTALL_DIR%"
+cd "%BIN_DIR%"
 
 @echo Starting Energi Core Node %VERSION%
 @echo Update your account information in the unlock parameter.
@@ -35,4 +36,4 @@ cd "%INSTALL_DIR%"
     --rpc^
     --rpcport 49796^
     --rpcaddr "127.0.0.1"^
-   	--verbosity 3 console 2>> %BLOCKCHAIN_DIR%\enegi3debug.log
+   	--verbosity 3 console 2>> %LOG_DIR%\enegi3debug.log
