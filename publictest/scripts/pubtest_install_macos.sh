3d655e04996c01172bb6580e1a4a4d20167b9b8c
zalam003 <48445346+zalam003@users.noreply.github.com>
2019-11-04 11:39:26 -0500

    Update pubtest_install_macos.sh

--- publictest/scripts/pubtest_install_macos.sh
+++ publictest/scripts/pubtest_install_macos.sh
@@ -105,6 +105,7 @@ case $? in
         mv energi3-${OSVER}-10.6-amd64 energi3-${OSVER}-10.6-amd64-${OLD_VERSION}
         curl https://s3-us-west-2.amazonaws.com/download.energi.software/releases/energi3/${VERSION}/energi3-${OSVER}-10.6-amd64 --output energi3-${OSVER}-10.6-amd64
         chmod +x energi3-${OSVER}-10.6-amd64
+        mv energi3-${OSVER}-10.6-amd64 energi3
         
         echo "Downloading staking script to start Energi Core Node server: run_macos.sh"
         curl -sL https://raw.githubusercontent.com/zalam003/EnergiCore3/master/publictest/scripts/run_macos.sh > run_macos.sh
