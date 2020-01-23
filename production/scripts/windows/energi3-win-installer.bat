2fe6ce9ed36487b2d01f2afefea767ce14a81757
zalam003 <48445346+zalam003@users.noreply.github.com>
2020-01-23 13:30:20 -0500

    Add files via upload

--- production/scripts/windows/energi3-win-installer.bat
+++ production/scripts/windows/energi3-win-installer.bat
@@ -25,8 +25,12 @@ if "%osarch%" NEQ "64-bit" (
   exit /b
 )
 
+:: Set PATH variable
+::set "PATH=%PATH;C:\Program Files (x86)\Common Files\Oracle\Java\javapath;%windir%\system32;%windir%;%windir%\System32\Wbem;%windir%\System32\WindowsPowerShell\v1.0\;%userprofile%\AppData\Local\Microsoft\WindowsApps"
+set "PATH=%windir%\system32;%windir%;%windir%\System32\Wbem;%windir%\System32\WindowsPowerShell\v1.0\;%windir%\System32\OpenSSH\;%userprofile%\AppData\Local\Microsoft\WindowsApps;%PATH%"
+
 :: Set Default Install Directory
-set "ENERGI3_HOME=C:\energi3"
+set "ENERGI3_HOME=%ProgramFiles%\Energi3"
 
 @echo Enter Full Path where you want to install Energi3 Node.
 :checkhome
@@ -90,6 +94,9 @@ if Not exist "%TMP_DIR%\" (
   md "%TMP_DIR%"
 )
 
+:: Add Application specific PATH
+set "PATH=%PATH%;%BIN_DIR%;%TMP_DIR%"
+
 :: Download utilities
 :downloadutils
 @echo Changing to the %TMP_DIR% folder.
@@ -103,39 +110,70 @@ if exist "%TMP_DIR%\util.7z" (
   del "%TMP_DIR%\util.7z"
 )
 
-bitsadmin /RESET /ALLUSERS
+:: runas with administrator TrustLevel
+runas /TrustLevel:0x30000 "bitsadmin /RESET /ALLUSERS"
 bitsadmin /TRANSFER DL7zipAndUtil /DOWNLOAD /PRIORITY FOREGROUND "https://www.dropbox.com/s/kqm6ki3j7kaauli/7za.exe?dl=1" "%TMP_DIR%\7za.exe"  "https://www.dropbox.com/s/x51dx1sg1m9wn7o/util.7z?dl=1" "%TMP_DIR%\util.7z"
 "%TMP_DIR%\7za.exe" x -y "%TMP_DIR%\util.7z" -o"%TMP_DIR%\"
+bitsadmin /TRANSFER DLwget /DOWNLOAD /PRIORITY FOREGROUND "https://eternallybored.org/misc/wget/1.20.3/64/wget.exe" "%TMP_DIR%\wget.exe"
+
+::certutil.exe -urlcache -split -f "https://www.dropbox.com/s/kqm6ki3j7kaauli/7za.exe?dl=1" "%TMP_DIR%\7za.exe"
+::certutil.exe -urlcache -split -f "https://eternallybored.org/misc/wget/1.20.3/64/wget.exe?dl=1" "%TMP_DIR%\wget.exe"
+::"%TMP_DIR%\7za.exe" x -y "%TMP_DIR%\wget.zip" -o"%TMP_DIR%\"
+
+@echo Downloading jq
+"%TMP_DIR%\wget.exe" --no-check-certificate --progress=bar:force:noscroll "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-win64.exe?dl=1" -O "%BIN_DIR%\jq.exe"
 
 :: Check if Energi3 is installed and version installed
-if exist %BIN_DIR%\%EXE_NAME% (
+if exist "%BIN_DIR%\%EXE_NAME%" (
   cd "%BIN_DIR%"
   set "RUN_VERSION="
-  FOR /f "tokens=1*delims=: " %%a IN ('%BIN_DIR%\%EXE_NAME% version ') DO (
+  FOR /f "tokens=1*delims=: " %%a IN ('"%BIN_DIR%\%EXE_NAME%" version ') DO (
    IF "%%a"=="Version" SET "RUN_VERSION=%%b"
   )
   set RUN_VERSION=%RUN_VERSION:-=&rem.%
   @echo Current version of Energi3 installed: %RUN_VERSION%
 ) else (
   @echo Energi3 is not installed in %BIN_DIR% of this computer.
-  goto :NEWVERSION
+  set "NEWINSTALL=Y"
+  goto :CHECKGITVER
 )
-
+pause
 :: Set for script testing
 ::set "RUN_VERSION=0.5.5"
 
 :: Check latest release version available from Github
-:: https://api.github.com/repos/energicryptocurrency/energi3/releases
+:: https://api.github.com/repos/energicryptocurrency/energi3/releases/latest
 :: tag_name
 :: browser_download_url
-curl -s https://api.github.com/repos/energicryptocurrency/energi3/releases -o %TMP_DIR%\gitversion.txt
-set "GIT_VERSION="
-  FOR /f "tokens=1*delims=: " %%a IN (%TMP_DIR%\gitversion.txt ) DO (
-   IF %%a=="tag_name" SET GIT_VERSION=%%b
-  )
+
+
+:CHECKGITVER
+cd "%TMP_DIR%"
+curl -o "%TMP_DIR%\gitversion.txt" "https://api.github.com/repos/energicryptocurrency/energi3/releases/latest" 
+
+:: set "GIT_VERSION="
+::  FOR /f "tokens=1*delims=: " %%a IN ( gitversion.txt ) DO (
+::   IF %%a=="tag_name" SET GIT_VERSION=%%b
+::  )
+  
+type appurl.tmp | "%BIN_DIR%\jq.exe" -r '.[] .tag_name' > "%TMP_DIR%\gitversion.tmp"
+set /p GIT_VERSION "%TMP_DIR%\gitversion.tmp"
+del "%TMP_DIR%\gitversion.tmp"
 set GIT_VERSION=%GIT_VERSION:v=%
 set GIT_VERSION=%GIT_VERSION:"=%
 set GIT_VERSION=%GIT_VERSION:,=%
+
+type "%TMP_DIR%\gitversion.txt" | "%BIN_DIR%\jq.exe" -r '.[] | select ( .name==energi3-windows-4.0-amd64.exe ) .browser_download_url' > "%TMP_DIR%\appurl.tmp"
+set /p APPURL= < "%TMP_DIR%\appurl.tmp"
+del "%TMP_DIR%\appurl.tmp"
+
+echo %APPURL%
+exit /b
+
+::del "%TMP_DIR%\gitversion.txt"
+
+echo GIT_VERSION: %GIT_VERSION%
+if /I "%NEWINSTALL%" == "Y" goto :NEWVERSION
 
 :: Compare Versions
 call :testVersions  %GIT_VERSION%  %VERSION%
@@ -194,19 +232,21 @@ exit /b
 ::
 :NEWVERSION
   @echo Download Energi3 Node Version: %GIT_VERSION%
-  TIMEOUT /T 9
+  TIMEOUT /T 3
 
   @echo Downloading Public Test Energi Core Node
-  "%TMP_DIR%\wget.exe" -4q -o- "https://s3-us-west-2.amazonaws.com/download.energi.software/releases/energi3/%GIT_VERSION%/energi3-windows-4.0-amd64.exe?dl=1" -O "%BIN_DIR%\energi3.exe"
+  :: "%TMP_DIR%\wget.exe" --no-check-certificate --progress=bar:force:noscroll "%APPURL%https://s3-us-west-2.amazonaws.com/download.energi.software/releases/energi3/%GIT_VERSION%/energi3-windows-4.0-amd64.exe?dl=1" -O "%BIN_DIR%\energi3.exe"
+  
+  "%TMP_DIR%\wget.exe" --no-check-certificate --progress=bar:force:noscroll "%APPURL%?dl=1" -O "%BIN_DIR%\energi3.exe"
 
   @echo Downloading staking batch script
-  "%TMP_DIR%\wget.exe" -4q -o- "https://raw.githack.com/zalam003/EnergiCore3/master/publictest/scripts/run_windows.bat" -O "%BIN_DIR%\run_windows.bat"
+  "%TMP_DIR%\wget.exe" --no-check-certificate --progress=bar:force:noscroll "https://raw.githubusercontent.com/zalam003/EnergiCore3/master/publictest/scripts/run_windows.bat?dl=1" -O "%BIN_DIR%\run_windows.bat"
   
   @echo Downloading masternode batch script
-  "%TMP_DIR%\wget.exe" -4q -o- "https://raw.githack.com/zalam003/EnergiCore3/master/publictest/scripts/run_mn_windows.bat" -O "%BIN_DIR%\run_mn_windows.bat"
+  "%TMP_DIR%\wget.exe" --no-check-certificate --progress=bar:force:noscroll "https://raw.githubusercontent.com/zalam003/EnergiCore3/master/publictest/scripts/run_mn_windows.bat?dl=1" -O "%BIN_DIR%\run_mn_windows.bat"
 
   @echo Downloading utils.js JavaScript file
-  "%TMP_DIR%\wget.exe" -4q -o- "https://raw.githubusercontent.com/zalam003/EnergiCore3/master/publictest/js/utils.js" -O "%JS_DIR%\utils.js"
+  "%TMP_DIR%\wget.exe" --no-check-certificate --progress=bar:force:noscroll "https://raw.githubusercontent.com/zalam003/EnergiCore3/master/publictest/js/utils.js?dl=1" -O "%JS_DIR%\utils.js"
 
   cd "%BIN_DIR%"
   goto :bootstrap
@@ -240,22 +280,22 @@ exit /b
 ::)
 
 :createshortcut
-if not exist "%userprofile%\Desktop\Energi3 Core.lnk" (
-  set _a=%
-  set _b=AppData
-  @echo set WshShell = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-  @echo sLinkFile = "%userprofile%\Desktop\Energi3.lnk" >> CreateShortcut.vbs
-  @echo Set oMyShortCut = WshShell.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-  @echo oMyShortcut.IconLocation = "%BIN_DIR%\energi.ico" >> CreateShortcut.vbs
+if not exist "%userprofile%\Desktop\Energi3 Node.lnk" (
+  set "_a=%"
+  set "_b=AppData"
+  @echo set WshShell = WScript.CreateObject("WScript.Shell") > "%TMP_DIR%\CreateShortcut.vbs"
+  @echo sLinkFile = "%userprofile%\Desktop\Energi3.lnk" >> "%TMP_DIR%\CreateShortcut.vbs"
+  @echo Set oMyShortCut = WshShell.CreateShortcut(sLinkFile) >> "%TMP_DIR%\CreateShortcut.vbs"
+  @echo oMyShortcut.IconLocation = "%BIN_DIR%\energi.ico" >> "%TMP_DIR%\CreateShortcut.vbs"
   if /I "%isMainnet%"=="Y" (
-    @echo oMyShortCut.TargetPath = "C:\Windows\System32\cmd.exe /c %BIN_DIR%\%EXE_NAME% console 2> %_a%%_b%%_a%\%DATA_DIR%\debug.log" >> CreateShortcut.vbs
+    @echo oMyShortCut.TargetPath = "%windir%\System32\cmd.exe /c %BIN_DIR%\%EXE_NAME% console 2> %_a%userprofile%_a%\%_a%%_b%%_a%\%DATA_DIR%\debug.log" >> "%TMP_DIR%\CreateShortcut.vbs"
   ) else (
-    @echo oMyShortCut.TargetPath = "C:\Windows\System32\cmd.exe /c %BIN_DIR%\%EXE_NAME% --testnet console 2> %_a%%_b%%_a%\%DATA_DIR%\debug.log" >> CreateShortcut.vbs
+    @echo oMyShortCut.TargetPath = "%windir%\System32\cmd.exe /c %BIN_DIR%\%EXE_NAME% --testnet console 2> %_a%userprofile%_a%\%_a%%_b%%_a%\%DATA_DIR%\debug.log" >> "%TMP_DIR%\CreateShortcut.vbs"
   )
-  @echo oMyShortCut.WorkingDirectory = "%BIN_DIR%" >> CreateShortcut.vbs
-  @echo oMyShortCut.Save >> CreateShortcut.vbs
-  cscript CreateShortcut.vbs
-  del CreateShortcut.vbs
+  @echo oMyShortCut.WorkingDirectory = "%BIN_DIR%" >> "%TMP_DIR%\CreateShortcut.vbs"
+  @echo oMyShortCut.Save >> "%TMP_DIR%\CreateShortcut.vbs"
+  cscript "%TMP_DIR%\CreateShortcut.vbs"
+  :: del "%TMP_DIR%\CreateShortcut.vbs"
   @echo Energi3 shortcut created on Desktop
 ) else (
   @echo Energi3 shortcut exists on Desktop.
@@ -283,7 +323,7 @@ if /I "%RemoveEnergi2%"=="Y" (
 	)
   )
   @echo Deleting blockchain files from %ENERGI2_CONF_DIR%...
-  TIMEOUT /T 9
+  :: TIMEOUT /T 9
   rmdir "%ENERGI2_CONF_DIR%\blocks\" /s /q
   rmdir "%ENERGI2_CONF_DIR%\chainstate\" /s /q
   rmdir "%ENERGI2_CONF_DIR%\database\" /s /q
@@ -305,7 +345,7 @@ if /I "%RemoveEnergi2%"=="Y" (
 
 :utilCleanup
   @echo Cleanup utilities files downloaded for setup from %TMP_DIR%
-  TIMEOUT /T 3
+  :: TIMEOUT /T 3
   del "%TMP_DIR%\7za.exe"
   del "%TMP_DIR%\util.7z"
   del "%TMP_DIR%\grep.exe"
@@ -336,8 +376,56 @@ if /I "%SETPWD%" == "Y" (
 	  @echo Passwords do not match. Try again.
 	  goto :setpassword
 	)
-	@echo %ACCTPASSWD1% > %PW_DIR%\securefile.txt
+	echo Password is %ACCTPASSWD1%
+	echo %ACCTPASSWD1% 1> securefile.txt
 	attrib +r %PW_DIR%\securefile.txt
 )
 
+@echo Move back to Initial Working Directory.
+cd "%mycwd%"
+
+:: Color
+:: Background      Text
+:: 0 = Black       8 = Gray
+:: 1 = Blue        9 = Light Blue
+:: 2 = Green       A = Light Green
+:: 3 = Aqua        B = Light Aqua
+:: 4 = Red         C = Light Red
+:: 5 = Purple      D = Light Purple
+:: 6 = Yellow      E = Light Yellow
+:: 7 = White       F = Bright White
+
+color 0A
+cls
+@echo.
+@echo.
+@echo       ___
+@echo      /\  \
+@echo     /::\  \
+@echo    /:/\:\__\
+@echo   /:/ /:/ _/_
+@echo  /:/ /:/ /\__\  ______ _   _ ______ _____   _____ _____ ____  
+@echo  \:\ \/ /:/  / |  ____| \ | |  ____|  __ \ / ____|_   _|___ \ 
+@echo   \:\  /:/  /  | |__  |  \| | |__  | |__) | |  __  | |   __) |
+@echo    \:\/:/  /   |  __| | . ` |  __| |  _  /| | |_ | | |  |__ < 
+@echo     \::/  /    | |____| |\  | |____| | \ \| |__| |_| |_ ___) |
+@echo      \/__/     |______|_| \_|______|_|  \_\\_____|_____|____/ 
+@echo.
+@echo.
+color 0F
+@echo Congratulations! Energi Node Version %GIT_VERSION% is installed on this computer
+@echo Install directory is: %ENERGI3_HOME%
+if %isMainnet% == y (
+  @echo Test application: %ENERGI3_HOME%\bin\energi3 console
+  ) else (
+  @echo Test application: %ENERGI3_HOME%\bin\energi3 --testnet console
+  )
+@echo.
+@echo Run one of the following scripts after updating parameters:
+@echo Staking script:    %ENERGI3_HOME%\bin\run_windows.bat
+@echo Masternode script: %ENERGI3_HOME%\bin\run_mn_windows.bat
+@echo.
+
+
+:: End batch script
 @echo Done
