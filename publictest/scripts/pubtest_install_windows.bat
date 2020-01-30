3f5c4d274e5b4ca0c87acbad6bda6dbfafe33d4b
zalam003 <48445346+zalam003@users.noreply.github.com>
2020-01-30 09:18:19 -0500

    Update pubtest_install_windows.bat

--- publictest/scripts/pubtest_install_windows.bat
+++ publictest/scripts/pubtest_install_windows.bat
@@ -12,9 +12,9 @@ setlocal ENABLEEXTENSIONS
 
 REM ###===> Update if needed
 REM # Save previous version
-set "OLD_VERSION=0.5.9"
+set "OLD_VERSION=0.6.0"
 REM # Set version to install
-set "VERSION=0.6.0"
+set "VERSION=0.7.1"
 
 set "INSTALL_DIR=C:\energi3"
 set "DATA_DIR=EnergiCore3\testnet\energi3"
