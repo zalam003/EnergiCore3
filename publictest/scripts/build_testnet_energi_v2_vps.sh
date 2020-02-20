9d1f3bfd7c15f0d30a0f96abbe586aec2813e297
zalam003 <48445346+zalam003@users.noreply.github.com>
2020-02-20 09:24:59 -0500

    Add files via upload

--- publictest/scripts/build_testnet_energi_v2_vps.sh
+++ publictest/scripts/build_testnet_energi_v2_vps.sh
@@ -9,58 +9,71 @@ bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/zalam003/EnergiCore3/master
 '
 #
 
-if [ ! -d $HOME/.energicore/testnet1 ]
-then
-        mkdir -p $HOME/.energicore/testnet1
-else
-        echo "$HOME/.energicore/testnet1 already exists..."
-fi
+TESTNETCONFDIR=${HOME}/.energicore/testnet1
 
-if [ ! -f $HOME/.energicore/testnet1/energi.conf ]
-then
-  echo "Setting up energi.conf"
+sudo apt-get install software-properties-common -yq
 
-cat << ENERGI_CONF | tee $HOME/.energicore/testnet1/energi.conf >/dev/null
-rpcuser=nrg_rpc
-rpcpassword=asdfa9jofinasidfaiqqwnnnd
-rpcbind=127.0.0.1
-rpcallowip=127.0.0.1
-rpcport=55711
-server=1
-daemon=1
-logtimestamps=1
-listen=1
-staking=1
-masternode=0
-#externalip=123.123.123.123:9797
-#bind=10.1.1.10:9797
-maxconnections=24
-ENERGI_CONF
-
-        
-else
-        echo "$HOME/.energicore/testnet1/energi.conf exists..."
-        echo
-fi
-
-sudo apt-get install software-properties-common -y
-
-sudo add-apt-repository ppa:bitcoin/bitcoin
+echo | sudo add-apt-repository ppa:bitcoin/bitcoin
 
 sudo apt-get update
 
 sudo apt install libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libevent-pthreads-2.1-6 libminiupnpc10 libzmq5 libdb4.8 libdb4.8++ unzip -y
 
-wget https://s3-us-west-2.amazonaws.com/download.energi.software/releases/energi/v2.2.1/energicore-2.2.1-linux.tar.gz
-tar -xvzf energicore-2.2.1-linux.tar.gz
-mv energicore-2.2.1 energi
-ls -l
-echo "export PATH=${PATH}:${HOME}/energi/bin" >> ~/.bashrc
+sudo apt-get install -y virtualenv python-virtualenv
+
+wget https://s3-us-west-2.amazonaws.com/download.energi.software/releases/energi/v2.3.0.3/energicore-2.3.0.3-linux.tar.gz
+tar -xvzf energicore-2.3.0.3-linux.tar.gz
+mv energicore-2.3.0 energi
 
 cd .energicore/testnet1
 unzip https://github.com/zalam003/EnergiCore3/releases/download/v2.2.1-testnet/testnet-bootstrap-20200209.zip
+
+EXTERNALIP=$( timeout --signal=SIGKILL 10s curl -s http://ipinfo.io/ip )
+HOSTIP=$( ip addr show | grep inet | grep -v "inet6" | grep -v 127.0.0.1 | grep -v "docker" | sed 's/\// /g' | awk '{ print $2 }' )
+
+if [ ! -d ${TESTNETCONFDIR} ]
+then
+        mkdir -p ${TESTNETCONFDIR}
+else
+        echo "$HOME/.energicore already exists..."
+fi
+
+if [ ! -f ${TESTNETCONFDIR}/energi.conf ]
+then
+  cat << NRGCONF | ${TESTNETCONFDIR}/energi.conf >/dev/null
+rpcuser=nrg_mig
+rpcpassword=fasdlfkjadfpndlvnpihhsodfhadfaaf
+rpcallowip=127.0.0.1
+rpcport=19796
+rpcbind=127.0.0.1
+bind=${HOSTIP}:19797
+listen=1
+server=1
+daemon=1
+staking=1
+printtodebug=0
+printtoconsole=0
+maxconnections=24
+externalip=${EXTERNALIP}:19797
+#masternode=1
+#masternodeprivkey=PRIVKEY
+NRGCONF
+
+else
+        echo "${TESTNETCONFDIR}/energi.conf exists..."
+        echo "Update any parameters necessary"
+fi
+
+echo "export PATH=${PATH}:${HOME}/energi/bin" >> ~/.bashrc
 
 source ~/.bashrc
 
 energid --testnet -daemon
 
+cd ${TESTNETCONFDIR}
+git clone https://github.com/energicryptocurrency/sentinel.git
+cd sentinel
+sed -i "s/\#energi_conf\=\/home\/dev\/.energicore/$TESTNETCONFDIR/g" sentinel.conf
+virtualenv venv
+venv/bin/pip install -r requirements.txt
+venv/bin/python bin/sentinel.py
